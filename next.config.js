3095259a8dfe755362d279f44c8023dae541bae0
max <maximillianeinstein@gmail.com>
2022-02-19 16:35:44 +0100

    update next config part 6

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
@@ -7,7 +7,7 @@ const nextConfig = {
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? '/fuhqu.github.io/' : '',
+  // assetPrefix: isProd ? '/fuhqu.github.io/' : '',
 }
 
 module.exports = nextConfig
