8bf8c540d9b0b453c6342867b505ca3402af09e0
max <maximillianeinstein@gmail.com>
2022-02-19 16:17:13 +0100

    update next config part 4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { Octokit } from 'octokit'
 import styles from '../styles/Home.module.css'
 
@@ -40,10 +39,7 @@ const Home: NextPage<{ gists: Gist[] }> = ({ gists }) => {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
+          Powered by Foo
         </a>
       </footer>
     </div>
